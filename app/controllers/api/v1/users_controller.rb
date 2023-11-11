7e5c027e1284dc172cbe6e662a798004e8ad75a5
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-11-11 16:09:40 +0200

    configure user authentication

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::UsersController < ApplicationController
+    # CSRF Token Validations SKIP
+    protect_from_forgery with: :null_session
+    def login
+      # Check available user
+      user = User.find_by(name: params[:name])
+      if user.nil?
+        render json: { status: 'error', message: 'User does not exist SignUp to continue.' }, status: 422
+      else
+        render status: 200, json: { message: 'Login success', status: 200, id: user.id }
+      end
+    end
+  
+    def register
+      user = User.find_by(name: params[:name])
+  
+      if user.nil?
+        user = User.new(name: params[:name])
+  
+        if user.save
+          render status: 201,
+                 json: { message: 'you are successfuly register.', id: user.id, name: user.name,
+                         status: 201 }
+        else
+          render status: 500, json: { message: 'Something went wrong please try again later.', status: 500 }
+        end
+      else
+        render status: 400, json: { message: 'User already exist.', id: user.id, name: user.name,
+                                    status: 400 }
+      end
+    end
+end
