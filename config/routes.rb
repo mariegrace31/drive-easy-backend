4d544e2dcda833bce605e789574f3cc3d4e993d6
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-14 12:12:28 +0530

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "/login", to:"users#login"
       post "/register", to:"users#register"
+      resources :cars, only: [:index, :show, :create, :destroy] 
     end
   end
 end
