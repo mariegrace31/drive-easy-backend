229e1c410f4759ec67ee689c9f1a6e7bdf7b328d
mariegrace <mariegracebmg@gmail.com>
2023-11-14 11:56:21 +0200

    Update reservation controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,16 +1,43 @@
 class Api::V1::ReservationsController < ApplicationController
+  # GET /reservations
   def index
+    @reservations = Reservation.includes(car: :user).all
+    render json: serialize_reservations(@reservations)
   end
 
-  def show
-  end
-
+  # POST /reservations
   def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
-  def update
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:car_id, :user_id, :city, :date)
   end
 
-  def destroy
+  def serialize_reservations(reservations)
+    reservations.as_json(
+      only: [:id, :city, :date],
+      include: {
+        car: {
+          only: [:id, :name, :model, :description, :img, :finance_fee, :total_amount, :duration],
+          include: {
+            user: {
+              only: [:id, :name]
+            }
+          }
+        },
+        user: {
+          only: [:id, :name]
+        }
+      }
+    )
   end
 end
