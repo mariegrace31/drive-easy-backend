4fc75f2b2ad00627cb550bc3d0a9c32b4322229d
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-14 12:45:28 +0530

    Add validation/association for the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :img, presence: true
+  validates :finance_fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :total_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :duration, numericality: { only_integer: true, greater_than: 0 }
 end
