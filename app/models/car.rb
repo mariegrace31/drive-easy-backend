7b3ca861923d05507974c26b39a4d88e6631b40d
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-21 20:26:49 +0530

    Add app/models/car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
   validates :model, presence: true
