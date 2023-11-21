6b46fed1f80f26a72f8e47cc04b3299fdb758b5f
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-21 20:27:11 +0530

    Add  app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :cars
-  has_many :reservations, class_name: 'Reservation', foreign_key: 'user_id'
+  has_many :cars, dependent: :destroy
+  has_many :reservations, class_name: 'Reservation', foreign_key: 'user_id', dependent: :destroy
 
-  validates :name, :full_name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :name, :full_name, presence: true, length: { maximum: 50 }
 end
